311c2084f86dfcf055116f5aca5935c85be440de
maxdickinson2 <max17dickinson@hotmail.com>
2023-02-10 16:17:35 -0800

    added navbar

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-document.querySelector('#bio').on('click', )
+const makeBio = () => {
+    // const bio = document.createElement('p')
+    // const bioText = ('testing testing testing')
+    document.getElementById('bioText').innerText = 'testing'
+
+}
+
+
+
+
+
+
+
+
+
+
+// buttons = () => {
+
+document.getElementById('bio').addEventListener('click', makeBio)
+
+// }
+
+// buttons()
+
+function navBar() {
+    const x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function java() {
+    const x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
