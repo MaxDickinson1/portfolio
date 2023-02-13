812bbcbf06d291a81f22cb4efe78d7771a1cacb8
maxdickinson2 <max17dickinson@hotmail.com>
2023-02-12 22:32:05 -0800

    added event listneners for hovering mouse

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 // const makeBio = () => {
-//     // const bio = document.createElement('p')
-//     // const bioText = ('testing testing testing')
 //     document.getElementById('bioText').innerText = 'testing'
 
 // }
-
-
-
-
-
-
 
 
 
@@ -19,32 +11,69 @@
 // document.getElementById('bio').addEventListener('click', makeBio)
 
 // }
-
 // buttons()
 
+
+
+
+
 function navBar() {
-    const navDisplay = document.getElementById("myTopnav");
-    if (navDisplay.className === "topnav") {
-      navDisplay.className += " responsive";
-    } else {
-      navDisplay.className = "topnav";
-    }
-  }
+    const navDisplay = document.getElementById("navBar");
+    navDisplay.classList.toggle('revealed')
+}
+
+
 
   function resumeButton() {
     const resDisplay = document.getElementById("resumeDiv");
-    if (resDisplay.style.display === "none") {
-      resDisplay.style.display = "block";
-    } else {
-      resDisplay.style.display = "none";
-    }
+    resDisplay.classList.toggle('revealed')
   }
 
   function projectsButton() {
-    const ProjectDisplay = document.getElementById("projectDiv");
-    if (ProjectDisplay.style.display === "none") {
-      ProjectDisplay.style.display = "block";
-    } else {
-      ProjectDisplay.style.display = "none";
-    }
+    const resDisplay = document.getElementById("projectDiv");
+    resDisplay.classList.toggle('revealed')
   }
+
+
+
+///////////////////////////////////////////////////
+//mouse hover event listener resume
+//////////////////////////////////////////////////
+//grabs the resume header  
+const resumeGrab = document.getElementById('resumeHeader')
+//adds event listenre for when mouse is hovering over header resume
+  resumeGrab.addEventListener('mouseover', e => {
+    const h2Text = document.getElementById('resumeHeader')
+//makes it underlined when mouse is of header resume
+    h2Text.style.textDecoration = 'underline'
+  })
+//undoes what above does
+  const resumeLeave = document.getElementById('resumeHeader')
+  resumeGrab.addEventListener('mouseleave', e => {
+    const h2Text = document.getElementById('resumeHeader')
+//when mouse isnt hover it isnt underlined
+    h2Text.style.textDecoration = 'none'
+  })
+
+///////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////
+//mouse over event listener projects
+/////////////////////////////////////////////////
+
+const projectsHover = document.getElementById('projectsHeader')
+projectsHover.addEventListener('mouseover', e => {
+  const h2Text = document.getElementById('projectsHeader')
+
+  h2Text.style.textDecoration = 'underline'
+})
+
+const projectsLeave = document.getElementById('projectsHeader')
+projectsLeave.addEventListener('mouseleave', e => {
+  const h2Text = document.getElementById('projectsHeader')
+
+  h2Text.style.textDecoration = 'none'
+})
+
+//////////////////////////////////////////////////
